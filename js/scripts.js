99a97cf21eeb5a57bdc2e337cff85bbe5ffd7586
Grzegorz Stachowiak <gregista88@gmail.com>
2019-04-04 09:51:36 +0200

    done

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,62 @@
 (function() {
 
+function createButton(){
+
+
+  var button = document.createElement("button");
+
+  button.classList.add("backToTop", "hidden");
+  button.textContent = "BACK TO TOP"
+  document.body.appendChild(button);
+
+  return button;
+
+}
+
+  var button = createButton();
+
+
+  function animateScroll() {
+
+    if( document.scrollingElement.scrollTop > 0) {
+        window.scrollBy(0, -5);
+        setTimeout(animateScroll, 10);
+    }
+
+}
+
+
+
+
+
+  button.addEventListener("click", function(e) {
+
+    e.stopPropagation();
+
+    animateScroll();
+
+
+  }, false);
+
+
+
+
+
+
+
+
+  window.addEventListener("scroll", function(e) {
+
+    if(document.scrollingElement.scrollTop >= 100) {
+        button.classList.remove("hidden");
+    } else {
+        button.classList.add("hidden");
+    }
+
+}, false);
+
+
+
 
 
 })();
